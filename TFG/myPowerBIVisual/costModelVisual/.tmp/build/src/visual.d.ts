7b27e8749cabdb3e33b5ef53ec2d611c94c9042d
andresgobernaUOC <andresgoberna@uoc.edu>
2025-06-03 13:05:28 +0200

    OK

--- TFG/myPowerBIVisual/costModelVisual/.tmp/build/src/visual.d.ts
+++
@@ -1,9 +0,0 @@
-import powerbi from "powerbi-visuals-api";
-import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
-import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
-import IVisual = powerbi.extensibility.visual.IVisual;
-export declare class Visual implements IVisual {
-    private target;
-    constructor(options: VisualConstructorOptions);
-    update(options: VisualUpdateOptions): void;
-}
