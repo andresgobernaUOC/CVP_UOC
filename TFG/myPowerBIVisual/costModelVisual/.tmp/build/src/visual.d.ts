c77cee193f31e6d560e57f3cddd676a25633ad43
andresgobernaUOC <andresgoberna@uoc.edu>
2025-06-03 12:47:12 +0200

    Abstract In today’s business world, strategic decisions rely heavily on data insights, yet storytelling and communication of complex models remain a weak link. While Business Intelligence (BI) tools excel at data visualization, they often lack the capabilities to represent complex statistical models effectively, especially in cost segregation analysis. Accurately separating fixed and variable costs and correlating them with revenue demands both statistical robustness and clear storytelling.  When visuals fail to convey meaning, decision-makers lose confidence in the insights, reducing their willingness to act on the findings.  This project tackles these challenges by developing a custom BI visual, integrating an adapted estimator with intuitive storytelling to ensure trustworthy, transparent, and actionable insights for strategic decision-making.  The roadmap is divided into four main phases. It begins with model selection, identifying the approach that best meets the requirements and necessary adaptations. Various datasets, including clean data, noisy data, and outliers are used to evaluate each model’s performance. Once the optimal model is chosen, the next step is assessing computational resources to determine whether calculations can be handled within the BI tool or require upstream processing. Based on this, the solution is implemented in the appropriate layer. Finally, a tailored visual is designed to communicate insights clearly and effectively to decision-makers.

---
+++ TFG/myPowerBIVisual/costModelVisual/.tmp/build/src/visual.d.ts
@@ -0,0 +1,9 @@
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+export declare class Visual implements IVisual {
+    private target;
+    constructor(options: VisualConstructorOptions);
+    update(options: VisualUpdateOptions): void;
+}
