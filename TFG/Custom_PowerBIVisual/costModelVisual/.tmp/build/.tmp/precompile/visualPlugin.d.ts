c56ead26e88f2ba0a1f7651d654181598821facb
andresgobernaUOC <andresgoberna@uoc.edu>
2025-06-03 13:12:11 +0200

    V20250603
    Ready for Submission

---
+++ TFG/Custom_PowerBIVisual/costModelVisual/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var costModelVisualCC57A86EE3354662871A1724816ABEF2_DEBUG: IVisualPlugin;
+export default costModelVisualCC57A86EE3354662871A1724816ABEF2_DEBUG;
